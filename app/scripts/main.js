649fa5dbfba24f37fd567919ba5491b73cae2514
marcin <mkp123cvb!>
2016-05-01 21:17:07 +0200

    Exercise 2 initialization.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,60 +4,20 @@
   window.onload = doSomething;
 
   function doSomething() {
-    //calculate(3, 2, '*');
 
 
-    var result = superCalculate([1, 4, 8, 12, 25], '+');
+    var result1 = toDollars(3.0);
+
     document.write(result);
+
+
+
+    function toDollars() {
+
+    }
+
+
+
   };
-
-  function calculate(number1, number2, operator) {
-    var result;
-
-    if (operator === '*') {
-      result = number1 * number2;
-    } else if (operator === '+') {
-      result = number1 + number2;
-    } else if (operator === '-') {
-      result = number1 - number2;
-    } else if (operator === '/') {
-      result = number1 / number2;
-    }
-
-    return result
-  }
-
-
-  function polacz(charsTable) {
-    var result = '';
-
-    for (var i = 0; i < charsTable.length; i++) {
-      result = result + charsTable[i];
-    }
-
-    return result;
-  }
-
-  function sum(numberTable) {
-    var result = 0;
-    for (var i = 0; i < numberTable.length; i++) {
-      result = result + numberTable[i]
-    }
-
-    return result
-
-  }
-
-  function superCalculate(sCalculateTable, operator) {
-    var result = sCalculateTable[0];
-
-    for (var i = 1; i < sCalculateTable.length; i++) {
-      result = calculate(result, sCalculateTable[i], operator);
-    }
-    return result;
-
-  }
-
-  return result
 
 })();
