d9154a01a1f661c82994e089711e9efc7b15f2db
lukasz.usarz <lukasz.usarz@gmail.com>
2016-05-04 13:51:24 +0200

    Small changes

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,15 +4,18 @@
   window.onload = main;
 
   function main() {
+    var PLNInput = document.getElementById('PLN');
+    var dollarsButton = document.getElementById('buttonDolars');
+    var dollarsInput = document.getElementById('przekonwertowane');
 
-
-    var result = toDollars(3.82684301);
-
-    document.write(result);
+    dollarsButton.onclick = function () {
+      var kwotaWPLN = +PLNInput.value;
+      var result = toDollars(kwotaWPLN);
+      dollarsInput.innerHTML = '' + result;
+    };
 
 
     function toDollars(pricePLN) {
-
       var result = pricePLN * 3.82684301;
       return result;
 
